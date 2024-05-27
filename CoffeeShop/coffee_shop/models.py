b370f10267796c1241d2c04822febbe57f89b6ab
lizard-b <lizardb82@gmail.com>
2024-05-27 16:39:10 +0300

    upd settings models db migrations done

--- CoffeeShop/coffee_shop/models.py
+++ CoffeeShop/coffee_shop/models.py
@@ -1,19 +1,46 @@
 from django.db import models
 
 
-class Orders(models.Model):
-    pass
-
-
-class Products(models.Model):
-    pass
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+    composition = models.TextField(default="Состав не указан")
 
 
 class Staff(models.Model):
-    pass
+    director = 'DI'
+    admin = 'AD'
+    cook = 'CO'
+    cashier = 'CA'
+    cleaner = 'CL'
+
+    POSITIONS = [
+        (director, 'Директор'),
+        (admin, 'Администратор'),
+        (cook, 'Повар'),
+        (cashier, 'Кассир'),
+        (cleaner, 'Уборщик')
+    ]
+    full_name = models.CharField(max_length=255)
+    duty = models.CharField(max_length=2,
+                            choices=POSITIONS,
+                            default=cashier)
+    labor_contract = models.IntegerField(default=0)
 
 
-class ProductOrder(models.Model):
-    pass
+class Orders(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='ProductsOrders')
+
+
+class ProductsOrders(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    in_order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
 
 # Create your models here.
