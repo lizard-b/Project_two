aded1ee99d9dccd211dfd5a92d734277a9d86372
lizardb <lizardb82@gmail.com>
2024-05-27 07:32:38 +0300

    added app coffeeshop upd db

---
+++ CoffeeShop/coffee_shop/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Orders(models.Model):
+    pass
+
+
+class Products(models.Model):
+    pass
+
+
+class Staff(models.Model):
+    pass
+
+
+class ProductOrder(models.Model):
+    pass
+
+# Create your models here.
