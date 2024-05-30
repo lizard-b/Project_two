8c8759134a0e2124192d1b0684be08b1891159e0
lizard-b <lizardb82@gmail.com>
2024-05-30 17:37:50 +0300

    db & command list upd

--- NewsPaper/list_of_commands.py
+++ NewsPaper/list_of_commands.py
@@ -4,7 +4,11 @@ User.objects.create_user('sweet_baby')
 
 User.objects.create_user('wild_fox')
 
-Author.objects.create('sweet_baby')
+Author.objects.create()
 
 Author.objects.create()
 
+Category.objects.create(name='Soft')
+Category.objects.create(name='Hardware')
+Category.objects.create(name='DataScience')
+Category.objects.create(name='Design')
