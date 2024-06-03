f0ed8f9bfdad954bad65268e6731c67918b46b06
lizard-b <lizardb82@gmail.com>
2024-06-03 11:45:42 +0300

    started with views.py

--- CoffeeShop/coffee_shop/models.py
+++ CoffeeShop/coffee_shop/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 from datetime import datetime, timezone
 from .resources import POSITIONS
+from django.core.validators import MinValueValidator
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField(default=0.0)
-    composition = models.TextField(default="Состав не указан")
+    name = models.CharField(max_length=50, unique=True)
+    price = models.FloatField(
+        validators=[MinValueValidator(0.0)],
+    )
+    composition = models.TextField()
+    description = models.TextField()
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0)],
+    )
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.CASCADE,
+        related_name='products',  # все продукты в категории будут доступны через поле products
+    )
+
+    def __str__(self):
+        return f'{self.name.title()}: {self.description[:20]}'
+
+
+# Категория, к которой будет привязываться товар
+class Category(models.Model):
+    # названия категорий тоже не должны повторяться
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name.title()
 
 
 class Staff(models.Model):
