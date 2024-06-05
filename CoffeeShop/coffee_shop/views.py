d676c4e26d2add76f1814493bda2db68662cefab
lizard-b <lizardb82@gmail.com>
2024-06-05 14:52:43 +0300

    views.py made corrections

--- CoffeeShop/coffee_shop/views.py
+++ CoffeeShop/coffee_shop/views.py
@@ -15,18 +15,9 @@ class ProductsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
-
-
-class ProductDetail(DetailView):
-    # Модель всё та же, но мы хотим получать информацию по отдельному товару
-    model = Product
-    # Используем другой шаблон — product.html
-    template_name = 'product.html'
-    # Название объекта, в котором будет выбранный пользователем продукт
-    context_object_name = 'product'
-
     # Метод get_context_data позволяет нам изменить набор данных,
     # который будет передан в шаблон.
+
     def get_context_data(self, **kwargs):
         # С помощью super() мы обращаемся к родительским классам
         # и вызываем у них метод get_context_data с теми же аргументами,
@@ -39,5 +30,15 @@ class ProductDetail(DetailView):
         # чтобы на её примере рассмотреть работу ещё одного фильтра.
         context['next_sale'] = None
         return context
+
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Product
+    # Используем другой шаблон — product.html
+    template_name = 'product.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'product'
+
 
 # Create your views here.
