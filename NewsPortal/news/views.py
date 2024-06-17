abc61389176d2488c210b2e6b028fae3a74fe7b6
lizard-b <lizardb82@gmail.com>
2024-06-17 14:51:28 +0300

    news.html and views.py corrected

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -20,17 +20,6 @@ class NewsList(ListView):
     context_object_name = 'news'
     paginate_by = 10
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        self.filterset = NewsFilter(self.request.GET, queryset)
-        return self.filterset.qs
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['-time_now'] = datetime.utcnow()
-        context['filterset'] = self.filterset
-        return context
-
 
 class PostDetail(DetailView):
 
