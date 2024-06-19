3cacbea5dd9d45ceca2a31495e7188984d72ad75
lizardb <lizardb82@gmail.com>
2024-06-19 12:37:25 +0300

    corrections made for email sending

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -9,7 +9,7 @@ class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     def __str__(self):
-        return self.user.username.title()
+        return self.user.username
 
     def update_rating(self):
         sum_post_rate = 0
@@ -39,7 +39,7 @@ class Category(models.Model):
     subscribers = models.ManyToManyField(User, related_name='categories')
 
     def __str__(self):
-        return self.name.title()
+        return self.name
 
 
 class Post(models.Model):
@@ -66,7 +66,7 @@ class Post(models.Model):
         return self.post_text[:124] + '...'
 
     def __str__(self):
-        return f'{self.title.title()}: {self.post_text[:124]}'
+        return f'{self.title}: {self.post_text[:124]}'
 
     def get_absolute_url(self):
         return reverse('post', args=[str(self.id)])
@@ -88,7 +88,7 @@ class Comment(models.Model):
         self.save()
 
     def __str__(self):
-        return f'{self.user.username.title()}: {self.comment_text}'
+        return f'{self.user.username}: {self.comment_text}'
 
 
 class PostCategory(models.Model):
@@ -96,6 +96,6 @@ class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.category.name.title()}: {self.post.title}'
+        return f'{self.category.name}: {self.post.title}'
 
 # Create your models here.
--- NewsPortal/news/signals.py
+++ NewsPortal/news/signals.py
@@ -9,36 +9,36 @@ from .models import *
 
 
 def send_notifications(preview, pk, title, subscribers):
-    html_content = render_to_string(
-        'post_created_email.html',
-        {
-            'text': preview,
-            'link': f'{settings.SITE_URL}/news/{pk}',
-            # 'subscriber': f'{}',
+    for subscriber in subscribers:
+        html_content = render_to_string(
+            'post_created_email.html',
+            {
+                'username': subscriber.username,
+                'text': preview,
+                'link': f'{settings.SITE_URL}/news/{pk}',
+            }
+        )
 
-        }
-    )
+        msg = EmailMultiAlternatives(
+            subject=title,
+            body='',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[subscriber.email],
 
-    msg = EmailMultiAlternatives(
-        subject=title,
-        body='',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to=subscribers,
+        )
 
-    )
-
-    msg.attach_alternative(html_content, 'text/html')
-    msg.send()
+        msg.attach_alternative(html_content, 'text/html')
+        msg.send()
 
 
 @receiver(m2m_changed, sender=PostCategory)
 def notify_about_new_post(sender, instance, **kwargs):
     if kwargs['action'] == 'post_add':
         category = instance.categories.all()
-        subscribers_emails = []
+        subscribers_users = []
 
         for cat in category:
             subscribers = cat.subscribers.all()
-            subscribers_emails += [s.email for s in subscribers]
+            subscribers_users += [s for s in subscribers]
 
-        send_notifications(instance.preview(), instance.pk, instance.title, subscribers_emails)
+        send_notifications(instance.preview(), instance.pk, instance.title, subscribers_users)
