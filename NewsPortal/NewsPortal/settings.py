b717b3fe3c2800fd403856636a2c27745b5bf674
lizard-b <lizardb82@gmail.com>
2024-06-20 18:54:13 +0300

    celery&redis local started

--- NewsPortal/NewsPortal/settings.py
+++ NewsPortal/NewsPortal/settings.py
@@ -191,4 +191,10 @@ DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_EMAIL')
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 APSCHEDULER_RUN_NOW_TIMEOUT = 25
 
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
