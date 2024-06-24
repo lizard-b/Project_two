64cd154d4544281b3eac4ef21a6c5d0254206fd0
lizardb <lizardb82@gmail.com>
2024-06-24 07:21:28 +0300

    celery periodic tasks

--- NewsPortal/NewsPortal/celery.py
+++ NewsPortal/NewsPortal/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPortal.settings')
 
@@ -7,3 +8,16 @@ app = Celery('NewsPortal')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'check_new_post_send_notifications_every_15_min': {
+        'task': 'news.tasks.notify_about_new_post',
+        'schedule': crontab(minute='*/15'),
+    },
+    'send_week_notification_every_week': {
+        'task': 'news.tasks.send_week_notification',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+    }
+}
