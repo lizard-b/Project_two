d0500e486cd7db1f1cb8961bdf90b74ba616f9ba
lizard-b <lizardb82@gmail.com>
2024-07-11 18:59:31 +0300

    admin panel upd

--- NewsPortal/news/admin.py
+++ NewsPortal/news/admin.py
@@ -12,10 +12,37 @@ class PostAdmin(admin.ModelAdmin):
         return "\n".join([p.name for p in obj.categories.all()])
 
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subscriber')
+    list_filter = ('name', 'subscribers')
+    search_fields = ('name', 'subscribers__username')
+
+    def subscriber(self, obj):
+        return ",\n".join([p.username for p in obj.subscribers.all()])
+
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'post'[:50])
+    list_filter = ('category', )
+    search_fields = ('category', 'post')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_rating')
+    list_filter = ('user', 'user_rating')
+    search_fields = ('user__username', 'user_rating')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment_text', 'comm_time_in', 'comment_rating', 'user',)
+    list_filter = ('comm_time_in', 'comment_rating', 'user',)
+    search_fields = ('user__username', 'comm_time_in', 'comment_rating', 'post__title')
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
-admin.site.register(PostCategory)
-admin.site.register(Author)
-admin.site.register(Comment)
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Comment, CommentAdmin)
 
 # Register your models here.
