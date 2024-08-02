cca2f525235901aa68181c85f37660c8577e4139
lizard-b <lizardb82@gmail.com>
2024-08-02 15:56:29 +0300

    tz started#2

--- NewsPortal/NewsPortal/settings.py
+++ NewsPortal/NewsPortal/settings.py
@@ -141,7 +141,7 @@ LANGUAGES = [
     ('ru', 'Русский')
 ]
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,10 +1,12 @@
 import datetime
 
+import pytz
 from django.shortcuts import redirect, get_object_or_404, render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.models import Group
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from django.utils import timezone
 from django.views.generic import (ListView, DetailView,
                                   CreateView, UpdateView, DeleteView, TemplateView, View)
 
