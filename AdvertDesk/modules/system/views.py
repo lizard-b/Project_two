125908117b519720520c5dec3ced8ca89ae107a4
lizard-b <lizardb82@gmail.com>
2024-08-28 08:55:34 +0300

    EMAIL cofirm started

--- AdvertDesk/modules/system/views.py
+++ AdvertDesk/modules/system/views.py
@@ -1,4 +1,10 @@
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.sites.models import Site
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.views.generic import DetailView, UpdateView, CreateView
 from django.contrib.auth.views import (LoginView, LogoutView,
                                        PasswordChangeView, PasswordResetView, PasswordResetConfirmView,
@@ -10,6 +16,7 @@ from .models import Profile
 from .forms import (UserUpdateForm, ProfileUpdateForm, UserRegisterForm,
                     UserLoginForm, UserPasswordChangeForm, UserForgotPasswordForm, UserSetNewPasswordForm,
                     )
+from ...AdvertDesk import settings
 
 
 class ProfileDetailView(DetailView):
@@ -69,13 +76,30 @@ class UserRegisterView(SuccessMessageMixin, CreateView):
     """
     form_class = UserRegisterForm
     success_url = reverse_lazy('adverts_home')
-    template_name = 'system/user_register.html'
-    success_message = 'Вы успешно зарегистрировались. Можете войти на сайт!'
+    template_name = 'system/registration/user_register.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Регистрация на сайте'
         return context
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        # Функционал для отправки письма и генерации токена
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        activation_url = reverse_lazy('confirm_email', kwargs={'uidb64': uid, 'token': token})
+        current_site = Site.objects.get_current().domain
+        send_mail(
+            'Подтвердите свой электронный адрес',
+            f'Пожалуйста, перейдите по следующей ссылке, чтобы подтвердить свой адрес электронной почты: http://{current_site}{activation_url}',
+            [settings.DEFAULT_FROM_EMAIL],
+            [user.email],
+            fail_silently=False,
+        )
+        return redirect('email_confirmation_sent')
 
 
 class UserLoginView(SuccessMessageMixin, LoginView):
